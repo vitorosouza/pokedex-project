c0f0cfe24e4152d10143b7d6eb9bda50c69b5c5f
vitorosouza <vitorosouza@hotmail.com>
2022-07-30 01:00:09 -0300

    Adiciona novos repositorios

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const fetchPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
+
+
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
+    ['animated']['front-default'];
+
+}
+
+renderPokemon('charizard');
